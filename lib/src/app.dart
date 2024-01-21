4540a221b06fa50e1b522986d41fe79aa59bcebf
Av3z <willyanf17@hotmail.com>
2024-01-20 23:24:36 -0300

    feat: added MyApp in App class and changed name for just APP

---
+++ lib/src/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:mycontacts/src/pages/home_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
