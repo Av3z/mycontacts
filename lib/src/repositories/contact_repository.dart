bb354c0d6694dd24d2d96524725a9e3585bf1e68
Av3z <willyanf17@hotmail.com>
2024-01-21 23:08:26 -0300

    chore: added dio options

--- lib/src/repositories/contact_repository.dart
+++ lib/src/repositories/contact_repository.dart
@@ -4,6 +4,8 @@ import 'package:mycontacts/src/models/contact_model.dart';
 class ContactRepository {
   Future<ContactModel> getContact() async {
     var dio = Dio();
+    dio.options.headers['X-Parse-Application-Id'] = "";
+    dio.options.headers['X-Parse-REST-API-Key'] = "";
     var result =
         await dio.get('https://parseapi.back4app.com/classes/contacts');
     return ContactModel.fromJson(result.data);
